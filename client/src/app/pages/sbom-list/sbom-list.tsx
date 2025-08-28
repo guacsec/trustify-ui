cd7440e5f921eeab1ec7be516787660809ce3b12
Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>
2025-08-28 13:29:54 +0200

    feat: add crud operations for importers
    Signed-off-by: Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>

--- client/src/app/pages/sbom-list/sbom-list.tsx
+++ client/src/app/pages/sbom-list/sbom-list.tsx
@@ -1,10 +1,17 @@
 import type React from "react";
 
-import { Content, PageSection } from "@patternfly/react-core";
+import {
+  Content,
+  PageSection,
+  Tab,
+  Tabs,
+  TabTitleText,
+} from "@patternfly/react-core";
 
 import { SbomSearchProvider } from "./sbom-context";
 import { SbomTable } from "./sbom-table";
 import { SbomToolbar } from "./sbom-toolbar";
+import { ImporterList } from "./components/ImporterList";
 
 export const SbomList: React.FC = () => {
   return (
@@ -15,12 +22,25 @@ export const SbomList: React.FC = () => {
         </Content>
       </PageSection>
       <PageSection hasBodyWrapper={false}>
-        <div>
-          <SbomSearchProvider>
-            <SbomToolbar showFilters />
-            <SbomTable />
-          </SbomSearchProvider>
-        </div>
+        <Tabs
+          defaultActiveKey={0}
+          aria-label="Tabs in the uncontrolled example"
+          role="region"
+        >
+          <Tab
+            eventKey={0}
+            title={<TabTitleText>All SBOMs</TabTitleText>}
+            aria-label="Uncontrolled ref content - users"
+          >
+            <SbomSearchProvider>
+              <SbomToolbar showFilters />
+              <SbomTable />
+            </SbomSearchProvider>
+          </Tab>
+          <Tab eventKey={1} title={<TabTitleText>SBOM Sources</TabTitleText>}>
+            <ImporterList />
+          </Tab>
+        </Tabs>
       </PageSection>
     </>
   );
