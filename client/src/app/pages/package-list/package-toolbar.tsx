12de05122f7769f4dd7b6cd78f36063a531f5404
Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>
2025-12-19 07:53:12 -0500

    feat: manage columns in package list page
    Signed-off-by: Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>

--- client/src/app/pages/package-list/package-toolbar.tsx
+++ client/src/app/pages/package-list/package-toolbar.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 
-import { Toolbar, ToolbarContent, ToolbarItem } from "@patternfly/react-core";
+import {
+  Toolbar,
+  ToolbarContent,
+  ToolbarGroup,
+  ToolbarItem,
+} from "@patternfly/react-core";
 
 import { FilterToolbar } from "@app/components/FilterToolbar";
 import { SimplePagination } from "@app/components/SimplePagination";
+import { ManageColumnsToolbar } from "@app/components/TableControls/ManageColumnsToolbar";
 
 import { PackageSearchContext } from "./package-context";
 
@@ -23,12 +29,20 @@ export const PackageToolbar: React.FC<PackageToolbarProps> = ({
       paginationToolbarItemProps,
       paginationProps,
     },
+    columnState,
   } = tableControls;
 
   return (
     <Toolbar {...toolbarProps} aria-label="package-toolbar">
       <ToolbarContent>
         {showFilters && <FilterToolbar {...filterToolbarProps} />}
+        <ToolbarGroup variant="action-group-plain">
+          <ManageColumnsToolbar
+            columns={columnState.columns}
+            setColumns={columnState.setColumns}
+            defaultColumns={columnState.defaultColumns}
+          />
+        </ToolbarGroup>
         <ToolbarItem {...paginationToolbarItemProps}>
           <SimplePagination
             idPrefix="package-table"
