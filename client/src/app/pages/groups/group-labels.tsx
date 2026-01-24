28202c3e8e3a5051c155c8f265682509adf81a5d
Bryan Ramos <bramos@redhat.com>
2026-01-23 21:19:18 -0500

    Add component for group labels
    Signed-off-by: Bryan Ramos <bramos@redhat.com>

---
+++ client/src/app/pages/groups/group-labels.tsx
@@ -0,0 +1,23 @@
+import { Label, LabelGroup } from '@patternfly/react-core';
+type Props = {
+  labels?: Record<string, string>;
+};
+
+// TODO: Change color based on label type
+export const GroupLabels = ({ labels }: Props) => {
+  if (!labels || Object.keys(labels) === 0) {
+    return null;
+  }
+
+  return (
+    <LabelGroup>
+      {
+        Object.entries(labels).map(([key, value]) => (
+          <Label key={key} color='blue'>
+            {key}: {value}
+          </Label>
+        ))
+      }
+    </LabelGroup>
+  );
+}
