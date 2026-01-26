fdefb11ab4073ef1b08b7fed3cccacb3f5a9f7d0
Bryan Ramos <bramos@redhat.com>
2026-01-26 17:43:02 -0500

    Handle label coloring and formatting
    Signed-off-by: Bryan Ramos <bramos@redhat.com>

--- client/src/app/pages/groups/group-labels.tsx
+++ client/src/app/pages/groups/group-labels.tsx
@@ -1,8 +1,18 @@
 import { Label, LabelGroup } from "@patternfly/react-core";
 type Props = {
-  labels?: Record<string, string>;
+  labels?: Record<string, string | null>;
 };
 
+type FormattedLabel = {
+  format: string;
+  color: "purple" | "blue";
+};
+
+function formatLabel(key: string, value: string | null): FormattedLabel {
+  const format = value ? `${key}: ${value}` : `${key}`;
+  const color = key === "Product" ? "purple" : "blue";
+  return { color, format };
+}
 // TODO: Change color based on label type
 export const GroupLabels = ({ labels }: Props) => {
   if (!labels || Object.keys(labels).length === 0) {
@@ -11,11 +21,14 @@ export const GroupLabels = ({ labels }: Props) => {
 
   return (
     <LabelGroup>
-      {Object.entries(labels).map(([key, value]) => (
-        <Label key={key} color="blue">
-          {key}: {value}
-        </Label>
-      ))}
+      {Object.entries(labels).map(([key, value]) => {
+        const { color, format } = formatLabel(key, value);
+        return (
+          <Label key={key} color={`${color}`}>
+            {format}
+          </Label>
+        );
+      })}
     </LabelGroup>
   );
 };
