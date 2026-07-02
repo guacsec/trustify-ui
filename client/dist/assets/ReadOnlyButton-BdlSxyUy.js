13406dab1809e48e4381f6deda0355604c2b9b90
carlosthe19916 <carlosthe19916@users.noreply.>
2026-07-02 19:00:16 +0000

    Checkin dist dir

--- client/dist/assets/ReadOnlyButton-BdlSxyUy.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./rolldown-runtime-QTnfLwEv.js";import{i as t,t as n}from"./react-B-P7c96U.js";import{ln as r}from"./StateError-mN1q9Mqy.js";import{o as i}from"./index-4sa_oiFG.js";var a=e(t(),1),o=n(),s=e=>{let{areMutationsDisabled:t}=a.useContext(i);return(0,o.jsx)(r,{...e,isDisabled:t})};export{s as t};
