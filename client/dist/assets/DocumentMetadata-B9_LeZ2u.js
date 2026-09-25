467dca95ab223adfb9ee1f88961285e5d13c7ecd
gildub <gildub@users.noreply.>
2026-09-25 09:08:44 +0000

    Checkin dist dir

---
+++ client/dist/assets/DocumentMetadata-B9_LeZ2u.js
@@ -0,0 +1,1 @@
+import{t as e}from"./react-D-7welKy.js";import{t}from"./useBranding-DSDnq3xY.js";var n=e(),r=({title:e})=>{let r=t().application.title;return(0,n.jsx)(`title`,{children:e?`${e} | ${r}`:r})};export{r as t};
